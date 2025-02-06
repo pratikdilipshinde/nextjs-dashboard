442285096b851fd0600a9be121b663c9e5f852f1
Pratik Dilip Shinde <pratikshindecareer@gmail.com>
2025-02-05 22:00:45 -0500

    Initial commit - Upload NextJS Dashboard Code

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({subsets:['latin']});
+
+export const lusitana = Lusitana({
+    subset:['latin'],
+    weight: ['400']
+    });
