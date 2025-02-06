ed5ef71b195a5adcabc736a4618ff10bbd2828fb
Pratik Dilip Shinde <pratikshindecareer@gmail.com>
2025-02-06 18:54:57 -0500

    Fonts file modified

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,6 +3,6 @@ import {Inter, Lusitana} from 'next/font/google';
 export const inter = Inter({subsets:['latin']});
 
 export const lusitana = Lusitana({
-    subset:['latin'],
-    weight: ['400']
+    subsets:['latin'],
+    weight: ['400'],
     });
